72d9f97c5df837cf515b937b7a0a1c87f9518945
Arymus <arymusr@gmail.com>
2024-10-21 01:02:13 -0400

    Add files via upload

---
+++ Websites for Change Project/script.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("form");
+const submit = document.getElementById("submit");
+
+function editFormText() {
+    form.innerHTML = "Submitted!"
+    form.classList.add("submitText");
+}
+
+submit.addEventListener("click", editFormText);
